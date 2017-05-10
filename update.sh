49c79f4d172b01cd5b191e9cf9ff5eeb525c9737
Shawn Ma <qsma@thoughtworks.com>
2017-05-10 14:16:27 +0800

    update

--- update.sh
+++ update.sh
@@ -10,8 +10,8 @@ fi
 versions=( "${versions[@]%/}" )
 
 
-for version in "${versions[@]}"; do	
-  repoPackage="https://apt.dockerproject.org/repo/dists/ubuntu-trusty/main/binary-amd64/Packages.gz"
+for version in "${versions[@]}"; do
+  repoPackage="https://apt.dockerproject.org/repo/dists/ubuntu-xenial/main/binary-amd64/Packages.gz"
   fullVersion="$(curl -fsSL "${repoPackage}" | gunzip | awk -v pkgname="docker-engine" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
 	(
 		set -x
